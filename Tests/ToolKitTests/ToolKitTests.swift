9ade91e44db008d973c3f769962cdd0e414133eb
cham-s <cc.attou@gmail.com>
2018-12-02 17:28:05 +0100

    testing travis

--- Tests/ToolKitTests/ToolKitTests.swift
+++ Tests/ToolKitTests/ToolKitTests.swift
@@ -5,6 +5,7 @@ final class ToolKitTests: XCTestCase {
     func testExample() {
         // This function test if the function returns the right path for the
         // the used documents URL
+        // testing travis
         let documentURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         
         XCTAssertEqual(documentURL, FileManager.default.documentDirectoryForCurrentUser)
